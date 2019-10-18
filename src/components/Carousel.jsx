f85edbfd1fc8471538633c8e3a0b3ce67a7617f5
Leonardo Flores <nadeshkokeane@hotmail.com>
2019-10-17 23:30:36 -0500

    added carousel components ans styles

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import '../assets/styles/components/Carousel.scss';
 
-Carousel = ({ children }) => (
+const Carousel = ({ children }) => (
   <section className='carousel'>
     <div className='carousel__container'>
       {children}
